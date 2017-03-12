7475c6d10fec29d63ccdab544b362c893d4eca1e
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2017-03-12 15:12:56 +0100

    Fixed example path in main.py

--- code/main.py
+++ code/main.py
@@ -2,7 +2,7 @@ import wave
 import matplotlib.pyplot as plt
 import numpy as np
 
-file = '/home/piotrek/dsp/docker/data/dataset/emotion-recognition-236f22a6fde0/4. dataset (audio)/Angry_all/You ' \
+file = '/home/piotrek/dsp/dataset/emotion-recognition-236f22a6fde0/4. dataset (audio)/Angry_all/You ' \
        + 'Oughta Know (Album Version).mp3.wav'
 spf = wave.open(file, 'r')
 signal = np.fromstring(spf.readframes(-1), 'Int16')
--- scripts/fetch.bash
+++ scripts/fetch.bash
@@ -6,5 +6,5 @@ ARCHIVE=archive.zip
 wget -O $ARCHIVE https://code.soundsoftware.ac.uk/hg/emotion-recognition/archive/tip.zip
 
 mkdir ../dataset
-unzip -d ../dataset $ARCHIVE
+unzip -q -d ../dataset $ARCHIVE
 rm $ARCHIVE
