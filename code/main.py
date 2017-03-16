079418dbb22b605dfe33394db0fc0b67f2bc72ee
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2017-03-16 22:43:55 +0100

    Listing all songs from one directory

--- code/main.py
+++ code/main.py
@@ -1,14 +1,24 @@
 import wave
 import matplotlib.pyplot as plt
 import numpy as np
+from os import listdir
 
-file = '/home/piotrek/dsp/dataset/emotion-recognition-236f22a6fde0/4. dataset (audio)/Angry_all/You ' \
-       + 'Oughta Know (Album Version).mp3.wav'
-spf = wave.open(file, 'r')
-signal = np.fromstring(spf.readframes(-1), 'Int16')
-spf.close()
 
-plt.figure(1)
-plt.plot(signal)
-plt.show()
-plt.interactive(False)
+def signal(path):
+    file = wave.open(path, 'r')
+    content = np.fromstring(file.readframes(-1), 'Int16')
+    file.close()
+    return content
+
+
+def plot(data):
+    plt.figure(1)
+    plt.plot(data)
+    plt.show()
+    plt.interactive(False)
+
+
+PATH = '../dataset/emotion-recognition-236f22a6fde0/4. dataset (audio)/Angry_all/'
+files = listdir(PATH)
+print(files)
+plot(signal(PATH + files[0]))
