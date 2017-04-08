7013a5f9f498d062f5e3dd5ac4fb67c49c91a16d
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2017-04-08 22:23:08 +0200

    Big refactor

---
+++ code/filesignals.py
@@ -0,0 +1,54 @@
+import os
+
+import numpy as np
+
+from FileSignal import FileSignal
+
+
+class FileSignals:
+    # @todo #0 duplicated ctors
+    def __init__(self, directory, db, fs):
+        self.directory = \
+            '../dataset/emotion-recognition-236f22a6fde0/4. dataset (audio)/' \
+            + directory
+        self.db = db
+        self.fs = fs
+
+    def signals(self):
+        return map(
+            lambda file:
+            FileSignal(self.directory + file, self.db, self.fs).value(),
+            os.listdir(self.directory)
+        )
+
+
+class DbSignals:
+    def __init__(self, directory, db, fs):
+        self.directory = \
+            '../dataset/emotion-recognition-236f22a6fde0/4. dataset (audio)/' \
+            + directory
+        self.db = db
+        self.fs = fs
+
+    class Do:
+        def __init__(self, directory, db, fs):
+            self.directory = directory
+            self.db = db
+            self.fs = fs
+
+        def __call__(self, file):
+            one = self.db.meta.find_one({"path": self.directory + file})
+            if not one:
+                raise Exception("There is no data for file {} in db.".format(
+                    self.directory + file
+                ))
+            return np.frombuffer(
+                self.fs.get(
+                    one["signal"]).read(), dtype='Int16')
+
+    def signals(self):
+        # @todo #0 try paraller here
+        return map(
+            DbSignals.Do(self.directory, self.db, self.fs),
+            os.listdir(self.directory)
+        )
--- code/main.py
+++ code/main.py
@@ -1,5 +1,31 @@
+import gridfs
+import pymongo
+
+from calculatingSummary import CalculatingSummary
+from filesignals import FileSignals, DbSignals
 from graph import Graph
 from mongoSummary import MongoSummary
 
+
+class SignalsFactory:
+    def __init__(self, use_db):
+        self.use_db = use_db
+        self.db = pymongo.MongoClient().signals
+
+    def create(self, path):
+        # @todo #0 reverse if
+        if not self.use_db:
+            return FileSignals(path,
+                               self.db,
+                               gridfs.GridFS(self.db))
+        else:
+            return DbSignals(path,
+                             self.db,
+                             gridfs.GridFS(self.db))
+
+
 if __name__ == "__main__":
-    MongoSummary(feature="random2").show_on(Graph())
+    MongoSummary(feature="random")
+    CalculatingSummary(
+        feature="random",
+        factory=SignalsFactory(False)).show_on(Graph())
