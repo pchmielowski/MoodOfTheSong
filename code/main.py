d777cf0feca73132d7ed5828deeb58d5ef970ad4
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2017-04-08 16:47:03 +0200

    style fixed

--- code/main.py
+++ code/main.py
@@ -1,23 +1,5 @@
-import gridfs
-import matplotlib.pyplot as plt
-from pymongo import MongoClient
-
 from graph import Graph
 from mongoSummary import MongoSummary
 
-db = MongoClient().gridfs_signals
-fs = gridfs.GridFS(db)
-
-
-# @todo #0 each class in another file
-
-
-def plot(data):
-    plt.figure(1)
-    plt.plot(data)
-    plt.show()
-    plt.interactive(False)
-
-
 if __name__ == "__main__":
-    MongoSummary("random2").show_on(Graph())
+    MongoSummary(feature="random2").show_on(Graph())
