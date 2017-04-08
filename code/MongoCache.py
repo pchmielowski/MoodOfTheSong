7d13da049a1e7c55addd45cf412d222f77f2e37e
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2017-04-08 14:03:55 +0200

    GridFS

--- code/MongoCache.py
+++ code/MongoCache.py
@@ -2,12 +2,11 @@ from pymongo import MongoClient
 
 
 class MongoCache:
-    def __init__(self):
-        self.mongo = MongoClient('mongodb://localhost:27017/').db.stats
+    def __init__(self, collection):
+        self.mongo = MongoClient('mongodb://localhost:27017/').db[collection]
 
-    def save(self, means, deviations):
-        self.mongo.insert_one({"means": means,
-                               "deviations": deviations})
+    def save(self, data):
+        self.mongo.insert_one(data)
 
     def read(self):
         return self.mongo.find_one()
