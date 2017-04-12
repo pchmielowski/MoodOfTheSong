d0861d880f900ed0ddc036b990a0b6188a4ef0ef
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2017-04-12 19:08:13 +0200

    Signals factory refactor

--- code/main.py
+++ code/main.py
@@ -9,17 +9,16 @@ class SignalsFactory:
     def __init__(self, use_db):
         self.use_db = use_db
         self.db = pymongo.MongoClient().signals
+        self.fs = gridfs.GridFS(self.db)
 
     def create(self, path):
-        # @todo #0 reverse if
-        if not self.use_db:
-            return FileSignals(path,
-                               self.db,
-                               gridfs.GridFS(self.db))
-        else:
+        if self.use_db:
             return DbSignals(path,
                              self.db,
-                             gridfs.GridFS(self.db))
+                             self.fs)
+        return FileSignals(path,
+                           self.db,
+                           self.fs)
 
 
 class Text:
